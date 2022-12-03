35fec2fad7f370068bd6be0b28956d7588f9675b
Astrium <romain.mellaza@orange.fr>
2022-12-03 22:45:49 +0100

    fix version

--- main.py
+++ main.py
@@ -9,11 +9,11 @@ from PIL import ImageTk
 root = tkinter.Tk()
 
 # Je défini des paramètres à cette fenêtre :
-root.title("Compresseur/décompresseur de fichiers")     # Un titre
+root.title("Compresseur/décompresseur de fichiers via la méthode Huffman")     # Un titre
 root.geometry("1080x720")                               # Un resolution d'affichage, ici HD
 root.minsize(1080, 720)                                 # Je bloque cette resolution, pour éviter que l'utilisateur ne redimmensionne n'importe comment.
 root.maxsize(1080, 720)
-#root.iconbitmap(default='icon\LOGO_resistance.ico')    # Je défini un icon pour la fenêtre
+root.iconbitmap(default='icon\icon_huffman_mellaza.ico')    # Je défini un icon pour la fenêtre
 
 
 
@@ -30,6 +30,7 @@ canvas_accueil.tag_lower(r,i)
 
 
 def compression():
+    global l,m,i,r,o,p,taux_bar
 
     filetypes = [
         ('Fichiers textes', '*.txt')
@@ -66,15 +67,24 @@ def compression():
     canvas_accueil.delete(r)
 
     try :
+        canvas_accueil.delete(g)
+        canvas_accueil.delete(h)
+    except :
+        pass
+
+    try :
+        canvas_accueil.delete(o)
+        canvas_accueil.delete(p)
         canvas_accueil.delete(l)
         canvas_accueil.delete(m)
-        canvas_accueil.delete(taux_bar)
+        taux_bar.destroy()
     except:
         pass
+
     
-    l=canvas_accueil.create_text(540.45, 137, text=" Compression effectuée avec succès ! ", font=("Helvetica", 42), fill="white", justify = CENTER)
-    m=canvas_accueil.create_rectangle(canvas_accueil.bbox(l),fill="#feb58a", width = 1)                                                            
-    canvas_accueil.tag_lower(m,l)
+    o=canvas_accueil.create_text(540.45, 137, text=" Compression effectuée avec succès ! ", font=("Helvetica", 42), fill="white", justify = CENTER)
+    p=canvas_accueil.create_rectangle(canvas_accueil.bbox(o),fill="#feb58a", width = 1)                                                            
+    canvas_accueil.tag_lower(p,o)
 
     l=canvas_accueil.create_text(540.45, 275, text=" Taux de compression ➔ " + str(taux) + " %", font=("Helvetica", 20), fill="white", justify = CENTER)
     m=canvas_accueil.create_rectangle(canvas_accueil.bbox(l),fill="#feb58a", width = 1)                                                            
@@ -85,9 +95,9 @@ def compression():
 
     taux_bar['value'] = taux
 
-#    print("\nTexte ASCII compressé à {:.2f} %".format(taux))
 
 def decompression():
+    global g,h
 
     filetypes = [
         ('Fichiers textes', '*.txt')
@@ -99,6 +109,21 @@ def decompression():
         filetypes=filetypes
     )
 
+    try :
+        canvas_accueil.delete(i)
+        canvas_accueil.delete(r)
+    except :
+        pass
+
+    try :
+        canvas_accueil.delete(o)
+        canvas_accueil.delete(p)
+        canvas_accueil.delete(l)
+        canvas_accueil.delete(m)
+        taux_bar.destroy()
+    except:
+        pass
+
     with open (filename, "r") as file_compressed:
         txt_file_work = file_compressed.read()
 
@@ -108,10 +133,16 @@ def decompression():
 
     phraseDecomp = decompresse(compressed_texte, encodage_huff)
 
+    g=canvas_accueil.create_text(540.45, 137, text=" Décompression effectuée avec succès ! ", font=("Helvetica", 42), fill="white", justify = CENTER)
+    h=canvas_accueil.create_rectangle(canvas_accueil.bbox(g),fill="#feb58a", width = 1)                                                            
+    canvas_accueil.tag_lower(h,g)
+
     filename_decompressed = os.path.splitext(filename)[0] + "_decompressed.txt"
 
     with open(filename_decompressed, 'w') as file_decompress:
         file_decompress.write(phraseDecomp)
+
+    
 
 # Définition du bouton pour sélectionner le mode de conversion de valeur à couleur :
 button_compression = Button(root, text="Compresser un fichier .txt", command=compression, font=("Helvetica", 26), fg='white', bg="#feb58a", height = 2, width = 24)
@@ -120,6 +151,22 @@ button_compression_window = canvas_accueil.create_window(30, 425, anchor='nw', w
 # Définition du bouton pour sélectionner le mode de conversion de couleur à valeur :
 button_decompression = Button(root, text="Décompresser un fichier .txt", command=decompression, font=("Helvetica", 26), fg='white', bg="#feb58a", height = 2, width = 24)
 button_decompression_window = canvas_accueil.create_window(560, 425, anchor='nw', window=button_decompression)
+
+
+
+def msg_remerciement():
+    '''
+    Cette procédure affiche un message de remerciement lorsque l'utilisateur ferme la fenêtre principale.
+    Puis elle met fin au fonctionnement de celle-ci.
+    '''
+    messagebox.showinfo('Dev : Romain MELLAZA',"Merci d'avoir utilisé mon logiciel ! :)")
+    root.destroy()
+
+# Ces lignes de codes permettent au programme d'actionner la fonction de remerciement si il reçoit l'information que l'utilisateur essaie de fermer le logiciel :
+try:
+    root.protocol('WM_DELETE_WINDOW', msg_remerciement)
+except:
+    pass
 
 
 # Je rafraîchis continuellement mon application via cette commande :
